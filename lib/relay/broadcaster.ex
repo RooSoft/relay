2733ec61e9b59cf91419d0d65e2446ab00068abc
marc@roosoft.com <marc@roosoft.com>
2023-02-03 20:16:44 -0500

    before broadcasting, filter by kind

--- lib/relay/broadcaster.ex
+++ lib/relay/broadcaster.ex
@@ -1,11 +1,22 @@
 defmodule Relay.Broadcaster do
-  alias Relay.Connection.SubscriptionRegistry
+  alias Relay.Connection.{Subscription, SubscriptionRegistry}
 
   def send(event) do
     IO.inspect(event, label: "BROADCASTING")
 
     for {pid, subscription} <- SubscriptionRegistry.lookup() do
-      send(pid, {:emit, subscription.id, event})
+      if matches_subscription(event, subscription) do
+        send(pid, {:emit, subscription.id, event})
+      end
     end
+  end
+
+  defp matches_subscription(event, subscription) do
+    event
+    |> filter_by_kind(subscription)
+  end
+
+  defp filter_by_kind(%{"kind" => kind} = event, %Subscription{kinds: kinds}) do
+    if Enum.member?(kinds, kind), do: event, else: nil
   end
 end
