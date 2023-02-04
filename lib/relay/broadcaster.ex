6dd49d893e5ec98099beaa46bff80ba7d95ef546
marc@roosoft.com <marc@roosoft.com>
2023-02-04 14:53:34 -0500

    can now filter notes by author

--- lib/relay/broadcaster.ex
+++ lib/relay/broadcaster.ex
@@ -15,13 +15,27 @@ defmodule Relay.Broadcaster do
     event
     |> filter_by_kind(subscription)
     |> filter_by_id(subscription)
+    |> filter_by_author(subscription)
   end
+
+  defp filter_by_kind(%{"kind" => kind} = event, %Subscription{kinds: nil}), do: event
 
   defp filter_by_kind(%{"kind" => kind} = event, %Subscription{kinds: kinds}) do
     if Enum.member?(kinds, kind), do: event, else: nil
   end
 
-  defp filter_by_id(%{"id" => id} = event, %Subscription{ids: ids}) do
+  defp filter_by_id(%{"kind" => kind} = event, %Subscription{ids: nil}), do: event
+
+  defp filter_by_id(%{"kind" => id} = event, %Subscription{ids: ids}) do
     if Enum.member?(ids, id), do: event, else: nil
+  end
+
+  defp filter_by_author(%{"pubkey" => author} = event, %Subscription{authors: nil}), do: event
+
+  defp filter_by_author(%{"pubkey" => author} = event, %Subscription{authors: authors}) do
+    IO.inspect(authors)
+    IO.inspect(author)
+
+    if Enum.member?(authors, author), do: event, else: nil
   end
 end
