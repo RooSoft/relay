8c4ff523deac42631db7d5c2d483a413705962cc
roosoft <marc@roosoft.com>
2023-02-16 09:13:15 -0500

    test add and remove a filter

--- test/relay/connection/filters_test.exs
+++ test/relay/connection/filters_test.exs
@@ -1,6 +1,8 @@
 defmodule Relay.Connection.FiltersTest do
   use ExUnit.Case, async: true
 
+  alias NostrBasics.Filter
+
   alias Relay.Connection.Filters
   alias Relay.Support
 
@@ -14,5 +16,17 @@ defmodule Relay.Connection.FiltersTest do
     filter_count = Filters.count()
 
     assert 1 == filter_count
+  end
+
+  test "add and remove a filter" do
+    %Filter{subscription_id: subscription_id} = filter = Support.FilterGenerator.new()
+
+    Filters.add(filter)
+
+    assert 1 == Filters.count()
+
+    Filters.remove(subscription_id)
+
+    assert 0 == Filters.count()
   end
 end
