1314581340e54b0333dcc5d58a7413139f6d4e34
roosoft <marc@roosoft.com>
2023-03-02 09:25:38 -0500

    test subscribe to a new registry, send a filter removal and receive it back

--- test/relay/nostr/filters/subscriptions_test.exs
+++ test/relay/nostr/filters/subscriptions_test.exs
@@ -7,18 +7,29 @@ defmodule Relay.Nostr.Filters.SubscriptionsTest do
 
   doctest Subscriptions
 
-  test "subscribe to a new registry, send a filter to it and receive it back" do
+  test "subscribe to a new registry, send a filter and receive it back" do
     registry_name = Generators.Atoms.generate()
     filter = Generators.Filter.new()
 
     Subscriptions.init(registry: registry_name)
-
     Subscriptions.subscribe(registry: registry_name)
-
     Subscriptions.dispatch_added_filter(filter, registry: registry_name)
 
     self_pid = self()
 
     assert_receive({:added_filter, ^self_pid, ^filter}, 1000)
+  end
+
+  test "subscribe to a new registry, send a filter removal and receive it back" do
+    registry_name = Generators.Atoms.generate()
+    subscription_id = Generators.Atoms.generate()
+
+    Subscriptions.init(registry: registry_name)
+    Subscriptions.subscribe(registry: registry_name)
+    Subscriptions.dispatch_removed_subscription(subscription_id, registry: registry_name)
+
+    self_pid = self()
+
+    assert_receive({:removed_subscription, ^self_pid, ^subscription_id}, 1000)
   end
 end
