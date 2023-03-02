4b09fe75daaea25c69b887a41c6f41436cb06ea3
roosoft <marc@roosoft.com>
2023-03-01 19:29:36 -0500

    enabled registry selection in Filters.Subscriptions

--- lib/relay/nostr/filters/subscriptions.ex
+++ lib/relay/nostr/filters/subscriptions.ex
@@ -1,37 +1,49 @@
 defmodule Relay.Nostr.Filters.Subscriptions do
-  def init() do
-    Registry.start_link(keys: :unique, name: Registry.FilterSubscribers)
+  @default_registry Registry.FilterSubscribers
+
+  def init(opts \\ []) do
+    registry = Enum.into(opts, %{}) |> Map.get(:registry, @default_registry)
+
+    Registry.start_link(keys: :unique, name: registry)
   end
 
-  def subscribe() do
-    Registry.register(Registry.FilterSubscribers, self(), self())
+  def subscribe(opts \\ []) do
+    registry = Enum.into(opts, %{}) |> Map.get(:registry, @default_registry)
+
+    Registry.register(registry, self(), self())
   end
 
-  def unsubscribe() do
-    Registry.unregister(Registry.FilterSubscribers, self())
+  def unsubscribe(opts \\ []) do
+    registry = Enum.into(opts, %{}) |> Map.get(:registry, @default_registry)
+
+    Registry.unregister(registry, self())
   end
 
-  def dispatch_added_filter(filter) do
-    dispatch({:added_filter, self(), filter})
+  def dispatch_added_filter(filter, opts \\ []) do
+    registry = Enum.into(opts, %{}) |> Map.get(:registry, @default_registry)
+
+    dispatch({:added_filter, self(), filter}, registry)
   end
 
-  def dispatch_removed_subscription(subscription_id) do
-    dispatch({:removed_subscription, self(), subscription_id})
+  def dispatch_removed_subscription(subscription_id, opts \\ []) do
+    registry = Enum.into(opts, %{}) |> Map.get(:registry, @default_registry)
+
+    dispatch({:removed_subscription, self(), subscription_id}, registry)
   end
 
-  defp dispatch(message) do
-    for pid <- get_all_filter_subscriber_pids() do
+  defp dispatch(message, registry) do
+    for pid <- get_all_filter_subscriber_pids(registry) do
       send(pid, message)
     end
   end
 
-  defp get_all_filter_subscriber_pids() do
+  defp get_all_filter_subscriber_pids(registry) do
     match_pattern = {:_, :"$1", :_}
     guards = []
     body = [{{:"$1"}}]
     spec = [{match_pattern, guards, body}]
 
-    Registry.select(Registry.FilterSubscribers, spec)
+    Registry.select(registry, spec)
     |> Enum.map(fn {pid} -> pid end)
     |> Enum.uniq()
   end
